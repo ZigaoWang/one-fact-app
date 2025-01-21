a727965b8b2e8f76a47fab33a17d6b07848d9422
Zigao Wang <zigao.wang24@gmail.com>
2025-01-21 20:13:15 +0800

    refractor: setup ios app

---
+++ One Fact/One Fact/One_FactApp.swift
@@ -0,0 +1,17 @@
+//
+//  One_FactApp.swift
+//  One Fact
+//
+//  Created by Zigao Wang on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct One_FactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
